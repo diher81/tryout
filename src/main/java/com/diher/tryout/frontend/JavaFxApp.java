4b55dd1a2551e3639f2e46b80f0eb51087797781
Dirk Hermans <diher@mac.home>
2024-12-12 21:28:13 +0100

    add java fx class and completed pom

---
+++ src/main/java/com/diher/tryout/frontend/JavaFxApp.java
@@ -0,0 +1,15 @@
+package com.diher.tryout.frontend;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.springframework.context.ApplicationContext;
+
+public class JavaFxApp extends Application {
+
+    private ApplicationContext springContext;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
