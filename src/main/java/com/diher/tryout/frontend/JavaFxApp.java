3516f856bcf44ba17963ad9bc9426d26ccc49bb8
Dirk Hermans <diher@mac.home>
2024-12-12 21:48:14 +0100

    show window

--- src/main/java/com/diher/tryout/frontend/JavaFxApp.java
+++ src/main/java/com/diher/tryout/frontend/JavaFxApp.java
@@ -1,15 +1,30 @@
 package com.diher.tryout.frontend;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import org.springframework.context.ApplicationContext;
 
 public class JavaFxApp extends Application {
 
     private ApplicationContext springContext;
+    private Button button;
 
     @Override
     public void start(Stage stage) throws Exception {
+        stage.setTitle("my first fx application");
+        button = new Button();
+        button.setText("knoppeke");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 200);
+        stage.setScene(scene);
+        stage.show();
+
 
     }
 }
